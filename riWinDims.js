16c02f018481dddfc0812b5752d7288d648f9396
Matthew Toledo <mrbinky3000@gmail.com>
2012-08-21 16:39:16 -0400

    Ran through jsLint. 0 warnings!  :-)

--- riWinDims.js
+++ riWinDims.js
@@ -1,6 +1,18 @@
-/*!  
- * riWinDims  
- *  
+/*jslint nomen: true, browser: true */
+/*global jQuery */
+/*properties
+    URL, addClass, ajax, animateResize, append, attr, bottom, breakPointUL,
+    clearInterval, css, data, 'data-windims-height', 'data-windims-width',
+    dataType, destroy, document, each, end, error, extend, find, firstInit,
+    floor, fn, getByUnique, getDims, height, hideResizer, html, htmlTemplate,
+    init, is, length, live, menu, name, on, open, opener, outerHeight,
+    outerWidth, prepend, preventDefault, qrCode, remove, removeClass, resizeTo,
+    riWinDims, setInterval, settings, showResizer, sizeMap, slideDown, slideUp,
+    start, stop, success, target, top, unbind, unique, updateWinDims, url, width
+*/
+/*!
+ * riWinDims
+ *
  * This jQuery plugin helps developers debug and present responsive website
  * prototypes to peers and clients.  It displays the current window dimensions, a
  * scan-able QR code, and options to resize the browser window.
@@ -10,34 +22,34 @@
  * client may not have them installed and you may not want to complicate things by
  * requiring your client to install them before viewing your work.
  *
- *  
+ *
  * @version 0.1.0
  * @date 8/20/2012
- * @author Matthew Toledo, John Keber  
- * @require jQuery v1.5+  
- */ 
- ;(function($) {     
- 	"use strict";
+ * @author Matthew Toledo, John Keber
+ * @require jQuery v1.5+
+ */
+(function ($) {
+    "use strict";
 
-    $.riWinDims = function(element, options) {
+    $.riWinDims = function (element, options) {
 
         var defaults = {
                 sizeMap: [
-                	{unique: 'fueg', menu: 0, start: 0,    end: 319,  name: 'small / text'},
-                	{unique: 'fumv', menu: 1, start: 320,  end: 320,  name: 'iPhone Portrait', height: 480},
-                	{unique: 'lknj', menu: 0, start: 321,  end: 479,  name: '&lt; iPhone Landscape'},
-                	{unique: 'n4js', menu: 1, start: 480,  end: 480,  name: 'iPone Landscape', height: 320},
-                	{unique: 'n0ep', menu: 0, start: 481,  end: 599,  name: '&lt; Kindle Nook'},
-					{unique: '4p3z', menu: 1, start: 600,  end: 600,  name: 'Kindle Nook Portrait', height: 800},
-					{unique: 'm1bs', menu: 0, start: 601,  end: 767,  name: '&lt; iPad Portrait'},
-					{unique: '2ouh', menu: 1, start: 768,  end: 768,  name: 'iPad Portrait', height: 1024},
-					{unique: 'cmth', menu: 0, start: 769,  end: 799,  name: '&lt; Kindle Nook Landscape'},
-					{unique: '39pc', menu: 1, start: 800,  end: 800,  name: 'Kindle Nook Landscape', height: 600},
-					{unique: 'ndue', menu: 0, start: 801,  end: 959,  name: '&lt; iPad Landscape'},
-					{unique: '3p7d', menu: 1, start: 960,  end: 960,  name: '960px', height: 680},
-					{unique: 'lmup', menu: 0, start: 961,  end: 1023, name: '&lt; iPad Landscape'},
-					{unique: 'izho', menu: 1, start: 1024, end: 1024, name: 'iPad Landscape', height: 768},
-					{unique: 'tb1d', menu: 0, start: 1025, end: 9999, name: 'Generic Desktop'}
+                    {unique: 'fueg', menu: 0, start: 0, end: 319, name: 'small / text'},
+                    {unique: 'fumv', menu: 1, start: 320, end: 320, name: 'iPhone Portrait', height: 480},
+                    {unique: 'lknj', menu: 0, start: 321, end: 479, name: '&lt; iPhone Landscape'},
+                    {unique: 'n4js', menu: 1, start: 480, end: 480, name: 'iPone Landscape', height: 320},
+                    {unique: 'n0ep', menu: 0, start: 481, end: 599, name: '&lt; Kindle Nook'},
+                    {unique: '4p3z', menu: 1, start: 600, end: 600, name: 'Kindle Nook Portrait', height: 800},
+                    {unique: 'm1bs', menu: 0, start: 601, end: 767, name: '&lt; iPad Portrait'},
+                    {unique: '2ouh', menu: 1, start: 768, end: 768, name: 'iPad Portrait', height: 1024},
+                    {unique: 'cmth', menu: 0, start: 769, end: 799, name: '&lt; Kindle Nook Landscape'},
+                    {unique: '39pc', menu: 1, start: 800, end: 800, name: 'Kindle Nook Landscape', height: 600},
+                    {unique: 'ndue', menu: 0, start: 801, end: 959, name: '&lt; iPad Landscape'},
+                    {unique: '3p7d', menu: 1, start: 960, end: 960, name: '960px', height: 680},
+                    {unique: 'lmup', menu: 0, start: 961, end: 1023, name: '&lt; iPad Landscape'},
+                    {unique: 'izho', menu: 1, start: 1024, end: 1024, name: 'iPad Landscape', height: 768},
+                    {unique: 'tb1d', menu: 0, start: 1025, end: 9999, name: 'Generic Desktop'}
                 ],
                 qrCode: 'https://chart.googleapis.com/chart?cht=qr&chs=100x100&chl=' + encodeURI(document.URL) + '&chld=L',
                 breakPointUL: '#ri-breakpoint-menu',
@@ -48,27 +60,27 @@
             winW,
             winH;
 
-        plugin.showResizer = function() {
+        plugin.showResizer = function () {
             $('#ri-sizer').html('-');
             $('#ri-winDims').addClass('ri-full-opacity');
-            $('#ri-sizes').stop(true,true).slideDown();
+            $('#ri-sizes').stop(true, true).slideDown();
         };
 
-        plugin.hideResizer = function() {
+        plugin.hideResizer = function () {
             $('#ri-sizer').html('+');
             $('#ri-winDims').removeClass('ri-full-opacity');
-            $('#ri-sizes').stop(true,true).slideUp();
+            $('#ri-sizes').stop(true, true).slideUp();
         };
 
         plugin.getDims = function () {
-            winW = parseInt($(window).width(),10);
-            winH = parseInt($(window).height(),10);
+            winW = parseInt($(window).width(), 10);
+            winH = parseInt($(window).height(), 10);
         };
 
         /**
          * Updates the readout with the latest window dimensions.  Highlight the current dimensions.
          */
-        plugin.updateWinDims = function() {
+        plugin.updateWinDims = function () {
 
             var menu,
                 $ul = $(plugin.settings.breakPointUL),
@@ -111,40 +123,40 @@
             throw new Error("No such sizeMap item with given unique id of " + unique);
         };
 
-        plugin.animateResize = function (startInnerWidth, startInnerHeight, targetInnerWidth,targetInnerHeight,steps,delay) {
+        plugin.animateResize = function (startInnerWidth, startInnerHeight, targetInnerWidth, targetInnerHeight, steps, delay) {
             var interval, widthSteps, heightSteps, i = 1,
 
-            _animateResize = function() {
-                var w, h,widthOffset, heightOffset;
+                _animateResize = function () {
+                    var w, h, widthOffset, heightOffset;
 
-                // window.resize() resizes the browser's outer width, not the width of the viewport.
-                // we need to compensate each iteration to account for appearing/disappearing scroll bars, etc.
-                if (window.outerWidth) {
-                   widthOffset = window.outerWidth - $(window).width();
-                   heightOffset = window.outerHeight - $(window).height();
-                }
+                    // window.resize() resizes the browser's outer width, not the width of the viewport.
+                    // we need to compensate each iteration to account for appearing/disappearing scroll bars, etc.
+                    if (window.outerWidth) {
+                        widthOffset = window.outerWidth - $(window).width();
+                        heightOffset = window.outerHeight - $(window).height();
+                    }
 
-                if (i === steps) {
-                    window.clearInterval(interval);
-                    window.resizeTo(targetInnerWidth + widthOffset, targetInnerHeight + heightOffset);
-                    plugin.getDims();
-                    plugin.updateWinDims();
-                    return;
-                }
+                    if (i === steps) {
+                        window.clearInterval(interval);
+                        window.resizeTo(targetInnerWidth + widthOffset, targetInnerHeight + heightOffset);
+                        plugin.getDims();
+                        plugin.updateWinDims();
+                        return;
+                    }
 
-                w = widthOffset + startInnerWidth + (widthSteps * i);
-                h = heightOffset + startInnerHeight + (heightSteps * i);
-                i++;
-                window.resizeTo(w,h);
+                    w = widthOffset + startInnerWidth + (widthSteps * i);
+                    h = heightOffset + startInnerHeight + (heightSteps * i);
+                    i += 1;
+                    window.resizeTo(w, h);
 
-            };
+                };
 
             widthSteps = Math.floor((targetInnerWidth - startInnerWidth) / steps);
             heightSteps = Math.floor((targetInnerHeight - startInnerHeight) / steps);
             //console.log('winW',winW,'winH',winH,'startInnerWidth',startInnerWidth,'startInnerHeight',startInnerHeight,'targetInnerWidth',targetInnerWidth,'targetInnerHeight',targetInnerHeight,'widthSteps',widthSteps,'heightSteps',heightSteps);
-            interval = window.setInterval(function(){
-                 _animateResize();
-            },delay);
+            interval = window.setInterval(function () {
+                _animateResize();
+            }, delay);
 
         };
 
@@ -153,21 +165,23 @@
          * Only called the first ever time this plugin is used.  This sets up global events and dom elements.
          * @private
          */
-        plugin.firstInit = function() {
+        plugin.firstInit = function () {
 
             var $li,
                 $ul,
-                i = plugin.settings.sizeMap.length -1,
-                o, newWidth, newHeight;
+                i = plugin.settings.sizeMap.length - 1,
+                o,
+                newWidth,
+                newHeight;
 
             // get initial dims potentially before the page is done loading to that we give them something
             plugin.getDims();
 
             // Create the HTML container.
             $.ajax({
-                url:plugin.settings.htmlTemplate,
-                dataType:'html',
-                success:function (data) {
+                url: plugin.settings.htmlTemplate,
+                dataType: 'html',
+                success: function (data) {
 
 
                     $('body').data('riWinDims', true).prepend(data);
@@ -207,7 +221,7 @@
                     });
 
                 },
-                error: function() {
+                error: function () {
                     throw new Error("riWinDims was unable to load the html template.");
                 }
 
@@ -218,13 +232,13 @@
             if (window.opener) {
                 newWidth = $("body", window.opener.document).data('windims-width');
                 newHeight = $("body", window.opener.document).data('windims-height');
-                plugin.animateResize(winW,winH,newWidth,newHeight,10,100);
+                plugin.animateResize(winW, winH, newWidth, newHeight, 10, 100);
             }
 
 
             // handle requests to resize the current window to specific dimensions
             // only works if the current window was created via javascript.
-            $('.ri-resizer').live('click',function(evnt){
+            $('.ri-resizer').live('click', function (evnt) {
                 var startInnerWidth = winW,
                     startInnerHeight = winH,
                     sizeObj = plugin.getByUnique($(this).data('unique')),
@@ -242,10 +256,10 @@
                         'data-windims-width': targetInnerWidth,
                         'data-windims-height': targetInnerHeight
                     });
-                    window.open(document.URL,'__blank__','width=200,height=300,location=yes,menubar=no,links=no,scrollbars=yes,toolbar=no,status=no');
+                    window.open(document.URL, '__blank__', 'width=200,height=300,location=yes,menubar=no,links=no,scrollbars=yes,toolbar=no,status=no');
                 } else {
                     plugin.getDims();
-                    plugin.animateResize(startInnerWidth,startInnerHeight,targetInnerWidth,targetInnerHeight,iSteps,time);
+                    plugin.animateResize(startInnerWidth, startInnerHeight, targetInnerWidth, targetInnerHeight, iSteps, time);
                     plugin.hideResizer();
                 }
                 return false;
@@ -253,7 +267,7 @@
             });
 
             // show or hide the resize options
-            $('#ri-sizer').live('click',function(ev){
+            $('#ri-sizer').live('click', function (ev) {
                 ev.preventDefault();
                 if ($('#ri-sizes').is(':hidden')) {
                     plugin.showResizer();
@@ -264,24 +278,24 @@
             });
 
             // move the bar from top to bottom and back again
-            $('#ri-locate').live('click',function(ev){
+            $('#ri-locate').live('click', function (ev) {
                 ev.preventDefault();
                 var $this = $(this);
                 $this = $(ev.target);
 
                 if ($this.attr('data-arrow') === 'd') {
                     $('#ri-winDims').css({
-                        'top':'auto',
-                        'bottom':'0'
+                        'top': 'auto',
+                        'bottom': '0'
                     });
-                    $this.attr('data-arrow','u');
+                    $this.attr('data-arrow', 'u');
                     $this.html('&#9650;');
                 } else {
                     $('#ri-winDims').css({
                         'top': '0',
-                        'bottom':'auto'
+                        'bottom': 'auto'
                     });
-                    $this.attr('data-arrow','d');
+                    $this.attr('data-arrow', 'd');
                     $this.html('&#9660;');
 
                 }
@@ -292,7 +306,7 @@
 
         plugin.settings = {};
 
-        plugin.init = function() {
+        plugin.init = function () {
 
             plugin.settings = $.extend({}, defaults, options);
 
@@ -316,18 +330,17 @@
         plugin.init();
 
 
-
     };
 
-    $.fn.riWinDims = function(options) {
+    $.fn.riWinDims = function (options) {
 
-        return this.each(function() {
-            if (undefined == $(this).data('riWinDims')) {
+        return this.each(function () {
+            if (undefined === $(this).data('riWinDims')) {
                 var plugin = new $.riWinDims(this, options);
                 $(this).data('riWinDims', plugin);
             }
         });
 
-    }
+    };
 
-})(jQuery);
+}(jQuery));
